724ae830bd45d65a6af6edad755d300b4ed52d57
moniradiu <moniradie@gmail.com>
2017-12-08 13:12:12 -0600

    Data Type

---
+++ app1/SecondClass.java
@@ -0,0 +1,30 @@
+package app1;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		// variable declarations
+		short FirstShortNumber;
+		int SecondIntNumber;
+		float ThiredFloatNumber;
+		
+		FirstShortNumber=10;
+		SecondIntNumber=20;
+		ThiredFloatNumber=40;
+		
+		//value with variable one line declaration.
+		int result=SecondIntNumber+10000;
+		float result2=ThiredFloatNumber/3;
+		char myCharacter='A';
+		boolean Statement=true;
+		
+		System.out.println(FirstShortNumber);
+		System.out.println(SecondIntNumber);
+		System.out.println(result);
+		System.out.println(result2);
+		System.out.println(myCharacter);
+		System.out.println(Statement);
+
+	}
+
+}
