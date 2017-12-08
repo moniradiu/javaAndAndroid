9c09efa1be639155f2b14a442bb40d3428f503c3
moniradiu <moniradie@gmail.com>
2017-12-08 14:32:38 -0600

    Array using with indexing and show.

---
+++ app1/Array.java
@@ -0,0 +1,17 @@
+package app1;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// Array declarations 
+		
+		//int[] cards=new int[8];
+//		 int[]cards={1,2,3,4,5};
+//		char[] cards={'1','2','3','4','5'};
+		char[] cards={'4','7','8','3','2','k'};
+		 			//0,1,2,3 indexing number show
+		 System.out.println(cards[2]);
+
+	}
+
+}
