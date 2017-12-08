d37fc86a59f693d89b556dcffa5b846f977078e8
moniradiu <moniradie@gmail.com>
2017-12-08 13:27:44 -0600

    Operator useing java

---
+++ app1/Operator.java
@@ -0,0 +1,28 @@
+package app1;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		// operator useing
+		
+		short FirstShortNumber;
+		int SecondIntNumber;
+		
+		FirstShortNumber=40;
+		SecondIntNumber=20;
+		
+		int result=FirstShortNumber+SecondIntNumber;
+		int result2=FirstShortNumber-SecondIntNumber;
+		int result3=FirstShortNumber*SecondIntNumber;
+		int result4=FirstShortNumber/SecondIntNumber;
+		int result5=FirstShortNumber%SecondIntNumber;
+		
+		System.out.println(result);
+		System.out.println(result2);
+		System.out.println(result3);
+		System.out.println(result4);
+		System.out.println(result5);
+
+	}
+
+}
