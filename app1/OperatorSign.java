6df8d14b7a6ac9ed1d735be2653a0e5da52ed71a
moniradiu <moniradie@gmail.com>
2017-12-08 14:03:11 -0600

    how to Operator using

---
+++ app1/OperatorSign.java
@@ -0,0 +1,45 @@
+package app1;
+
+public class OperatorSign {
+
+	public static void main(String[] args) {
+		// operator sign using
+		
+		
+		short FirstShortNumber;
+		int SecondIntNumber;
+		
+		FirstShortNumber=40;
+		SecondIntNumber=20;
+		
+		SecondIntNumber=SecondIntNumber+3;
+		//same thing up plus system.
+		
+		SecondIntNumber+=10;
+		
+//		SecondIntNumber-=10;
+//		SecondIntNumber/=2;
+		
+		//boolean using
+		boolean satement=(4)!=4;
+//		boolean satement=(5)!=4;
+//		boolean satement=(5)==4;
+//		boolean satement=(5)<4;
+//		boolean satement=10<4;
+//		boolean satement=3<4;
+		
+		//Increment and Decrement
+		FirstShortNumber++; //FirstShortNumber=FirstShortNumber+1; FirstShortNumber+=1;
+		//FirstShortNumber--; //FirstShortNumber=FirstShortNumber-1; FirstShortNumber-=1;
+		
+		
+		System.out.println(FirstShortNumber);
+		System.out.println(SecondIntNumber);
+		System.out.println(satement);
+		
+		
+
+
+	}
+
+}
